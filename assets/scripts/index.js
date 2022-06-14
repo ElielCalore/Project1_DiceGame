54825e0ab85874cdc06e7c36be15ef239090af8c
eliel <elieldscalore@gmail.com>
2022-06-14 09:55:27 -0300

    sound

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,18 +5,24 @@ const buttonRight = document.getElementById("buttonRightPosition");
 let index = 0;
 let counter = 0;
 
+const url = "./assets/sounds/sound_dice_roll.mp3";
+const audio = new Audio(url);
+
 buttonLeft.addEventListener("click", (event) => {
     event.preventDefault();
+    audio.play();
     counter++;
-    if (counter)
-        if (counter <= 2) {
-            newDice.diceShuffler(index);
-        } else if (counter <= 4) {
+    if (counter) audio.play();
+    if (counter <= 2) {
+        newDice.diceShuffler(index);
+        audio.play();
+    } else if (counter <= 4) {
         buttonLeft.disabled = true;
         newDice.diceShuffler(index + 1);
         buttonLeft.disabled = false;
     } else {
         counter = 0;
+        audio.play();
     }
 });
 
